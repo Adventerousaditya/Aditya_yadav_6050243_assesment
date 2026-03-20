dfedc0bb9a122e82ed1f3a961619659a2558e95c
adityayadav2212 <131519503+Adventerousaditya@users.noreply.github.com>
2026-03-20 13:36:27 +0530

    Add files via upload

---
+++ java/com/example/movieTrackAPI/MovieTrackApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.movieTrackAPI;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTrackApiApplication.class, args);
+        System.out.println("Spring Boot Track App is running...");
+    }
+}
